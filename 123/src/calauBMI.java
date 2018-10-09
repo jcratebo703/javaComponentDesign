4504042ba6ec347467ff6dc382d4daa9b83a790d
Mufeng <jcratebo703@gmail.com>
2018-10-09 15:09:07 +0800

    add calculate BMI java code

---
+++ 123/src/calauBMI.java
@@ -0,0 +1,24 @@
+
+/** */
+public class calauBMI {
+	/** */
+	private double w;
+	
+	/** */
+	private double h;
+	
+	/** */
+	public double getBMI() {
+	 return w/h*h;
+	}
+	
+	/** */
+	public void setW(double w) {
+		this.w = w;
+	}
+	
+	/** */
+	public void setH(double h) {
+		this.h = h;
+	}
+}
